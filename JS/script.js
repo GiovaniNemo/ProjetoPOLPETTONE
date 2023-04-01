f916b76b5854950ed65001aeb3e71ad64eb4c3c8
Giovani <sgiovanimendes@gmail.com>
2023-04-01 10:26:47 -0300

    att 0.1.7

--- JS/script.js
+++ JS/script.js
@@ -5,3 +5,11 @@ function clickMenu() {
     menu.style.display = 'block'
   }
 }
+
+function mudouTamanho() {
+  if (window.innerWidth >= 600) {
+    menu.style.display = 'block'
+  } else {
+    menu.style.display = 'none'
+  }
+}
